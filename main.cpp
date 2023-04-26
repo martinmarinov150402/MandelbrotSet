aa764cef22a1ac4d3a539fa6ca8eb1ad90ec4bf3
Martin Marinov <martinmarinov150402@gmail.com>
2023-04-26 23:57:38 +0300

    Not so successfull attempt but good for start

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 const int WIDTH = 800, HEIGHT = 800;
 const long double minimum = -2.0;
 const long double maximum = 2.0; 
+const int TRANSLATION_X = 300;
+const int TRANSLATION_Y = 100;
 const int MAX_ITERATIONS = 200;
 
 long double mapValue(long double val, long double in_min, long double in_max, long double out_min, long double out_max)
@@ -14,12 +16,12 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1500,1000), "Mandelbrot Set");
     window.clear();
     window.display();
-    for(int x = 0; x < WIDTH; x++)
+    for(int x = TRANSLATION_X; x < WIDTH + TRANSLATION_X; x++)
     {
-        for(int y = 0; y < HEIGHT; y++)
+        for(int y = TRANSLATION_Y; y < HEIGHT + TRANSLATION_Y; y++)
         {
-            long double newX = mapValue(x, 0, WIDTH, minimum, maximum);
-            long double newY = mapValue(y, 0, HEIGHT, minimum, maximum);
+            long double newX = mapValue(x - TRANSLATION_X, 0, WIDTH, minimum, maximum);
+            long double newY = mapValue(y - TRANSLATION_Y, 0, HEIGHT, minimum, maximum);
 
             long double xInitial = newX;
             long double yInitial = newY;
@@ -41,15 +43,17 @@ int main()
                 }
                 iterations++;
             }
+            int brightness = mapValue(iterations, 0, MAX_ITERATIONS, 0, 255);
             if(iterations == MAX_ITERATIONS)
             {
-                sf::Vertex point(sf::Vector2f(x,y), sf::Color::White);
-                window.draw(&point, 1, sf::Points);
+                brightness = 0;   
             }
+            sf::Vertex point(sf::Vector2f(x,y), sf::Color(brightness*100, brightness*150, brightness*200));
+            window.draw(&point, 1, sf::Points);
         }
     }
-    window.display();
 
+    window.display();
 
     //Event handling
     while(window.isOpen())
